eb85c5df0c9ca6842d82a0734a2a40d49cf18bf3
Stephanie Su <stephanie.fenhua@gmail.com>
2024-11-09 17:56:45 -0500

    create functional control buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { generateBoard } from './utils/generateBoard';
+import SudokuBoard from './components/SudokuBoard';
+import Controls from './components/Controls';
 
 function App() {
+  const {puzzle, solution} = generateBoard();
+  const [board, setBoard] = useState(puzzle);
+  const [originalBoard, setOriginalBoard] = useState(() => JSON.parse(JSON.stringify(puzzle)));
+  const [solvedBoard, setSolvedBoard] = useState(solution);
+  //console.log("board", board);
+  //console.log("originalBoard", originalBoard);
+  console.log("solvedBoard", solvedBoard);
+
+  const handleNewPuzzle = () => {
+    const {puzzle, solution} = generateBoard();
+    setBoard(puzzle);
+    setOriginalBoard(JSON.parse(JSON.stringify(puzzle)));
+    setSolvedBoard(solution);
+  };
+
+  const handleReset = () => {
+    setBoard(JSON.parse(JSON.stringify(originalBoard)));
+  };
+
+  const handleSolve = () => {
+    setBoard(solvedBoard);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I'm working on my first React App!
-          Edit <code>src/App.js</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sudoku</h1>
+      <SudokuBoard board={board} setBoard={setBoard} />
+      <Controls onNewPuzzle={handleNewPuzzle} onReset={handleReset} onSolve={handleSolve} />
     </div>
   );
 }
